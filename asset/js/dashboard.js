9912d51266df256d3728a6a373b753af04a95565
1660765 <1660765@student.hcmus.edu.vn>
2018-12-21 17:16:15 +0700

    added zoom in/out image status

--- asset/js/dashboard.js
+++ asset/js/dashboard.js
@@ -1,9 +1,36 @@
 $(document).ready(() => {
 
+	// Giả lập input upload file bằng button
 	$('#status-image-btn').on('click', (e) => {
 		e.preventDefault();
 
 		$('#status-image').click();
 	})
 
+
+	// Zoom image
+	$(".image_status").on("click", (e) => {
+		
+		onClickImageStatus(e);
+	});
+
 })
+
+function onClickImageStatus(e) {
+
+	// Focus background
+	$("body").addClass("focus");
+
+	// Zoom in image
+	$(e.target).addClass("center-image");
+
+	// Outside and zoomout image
+	$("body").click((body) => {
+		if(!$(body.target).is(".image_status")) {
+
+			$(e.target).removeClass("center-image");
+			$("body").removeClass("focus");
+
+		}
+	})
+}
