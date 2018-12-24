328fd65220a0225705b8cf44bb5b449e07441495
loinp <nploi1998@gmail.com>
2018-12-24 15:43:56 +0700

    Remove email

--- asset/search/search.js
+++ asset/search/search.js
@@ -10,8 +10,8 @@ function openSearchFilter(evt, filterName) {
     }
     tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < x.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+      tablinks[i].className = tablinks[i].className.replace(" w3-grey", "");
     }
     document.getElementById(filterName).style.display = "block";
-    evt.currentTarget.className += " w3-red";
+    evt.currentTarget.className += " w3-grey";
   }
