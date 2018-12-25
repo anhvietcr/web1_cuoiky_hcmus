0650eca685685df7751dd5b100a87b5e0bda63e4
1660765 <1660765@student.hcmus.edu.vn>
2018-12-25 22:11:21 +0700

    upload static database connection class

---
+++ inc/db.php
@@ -0,0 +1,39 @@
+<?php
+
+class db
+{
+    /*
+     *
+     * Nếu xây dựng các query trong class db thì chuyển $connection vể private
+     * Hiện tại để public là để các class khác có thể truy cập và thực thi
+     * query từ các class đó.
+     *
+     * */
+    private static $option;
+    public static $connection;
+
+    public static function connect()
+    {
+
+    	$host = 'localhost';
+    	$dbname = 'web1_hcmus';
+    	$username = 'root';
+    	$password = '123456';
+
+        self::$option = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false
+        ];
+        try {
+            if (!isset(self::$connection)) {
+                self::$connection = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8",
+                    $username,
+                    $password,
+                    self::$option);
+            }
+        } catch (PDOException $ex) {
+            throw new PDOException($ex->getMessage());
+        }
+    }
+}
