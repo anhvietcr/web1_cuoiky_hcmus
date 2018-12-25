523bd9e7d3268091b7fea0f0df6d89eab21c7fda
1660765 <1660765@student.hcmus.edu.vn>
2018-12-25 15:51:04 +0700

    fixed search.js selector btn class with null

--- asset/search/search.js
+++ asset/search/search.js
@@ -16,11 +16,14 @@ function filterSelection(c) {
 
 
 var btnContainer = document.getElementById("btnFilters");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
+
+if (location.href.indexOf('search.php') >= 0) {
+  var btns = btnContainer.getElementsByClassName("btn");
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function(){
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }  
 }
